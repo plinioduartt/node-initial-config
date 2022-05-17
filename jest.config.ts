354b6bb75b6097628788b2611af439aaa7eb85bb
plinioduartt <plinio.duartes@hotmail.com>
2022-05-17 00:23:58 -0300

    add jest

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+export default {
+  roots: ['<rootDir>/src'],
+  bail: 0,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  transform: {
+    '.*\\.ts$': 'ts-jest'
+  }
+}
